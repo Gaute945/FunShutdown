037e1e445e00e3fe7df857c4cb70565c4a34d128
mrboobai <e-gaugro@iskule.no>
2023-01-14 22:47:30 +0100

    live

--- Funshutdown.ps1
+++ Funshutdown.ps1
@@ -4,4 +4,4 @@ for ($i = 1; $i -lt 4; $i++) {
     Start-Process -FilePath "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" -ArgumentList "C:\Users\MrBoobi\OneDrive - Vestland fylkeskommune\Konsept utv & prog\Prog\Root\FunShutdown\pipes.mp3"
     Start-Sleep(1);
 }
-#shutdown /s
+shutdown /s
