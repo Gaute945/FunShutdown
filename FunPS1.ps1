e4d7b0f0f88483b87f40e547e908c109277fcbd8
mrboobai <e-gaugro@iskule.no>
2023-01-14 16:17:51 +0100

    sync

--- FunPS1.ps1
+++ FunPS1.ps1
@@ -1,6 +1,7 @@
 for ($i = 1; $i -lt 4; $i++) {
-    Start-Sleep(2);
+    Start-Sleep(3);
     Write-Host "shutdown in " $i
     Start-Process -FilePath "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" -ArgumentList .\pipes.mp3
+    Start-Sleep(1);
 }
 shutdown /s
